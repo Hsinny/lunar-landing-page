95d755f7e838f17a496b194bea78e6e3e2a4c3d0
Hsinny <hsinnyliu@gmail.com>
2023-05-05 01:05:59 +0800

    [temp] 圖片簡易使用方式

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,25 @@ import Image from 'next/image';
 
 export default function About() {
   return (
-    <main>About</main>
+    <main>
+      {/* 位在第一屏需要儘早載入的圖片加上 priority 屬性，會優先預加載 */}
+      <Image
+        className="bg-white"
+        src="/next.svg"
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+        priority
+      />
+
+      {/* "非"在第一屏的圖，不用給 priority 屬性，會自動 Lazy Loading 載入 */}
+      <Image
+        className="bg-white"
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        width={180}
+        height={40}
+      />
+    </main>
   )
 }
