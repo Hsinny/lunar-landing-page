bcfcd41c68d08b4fbeacefa98f4604cbccc43d5f
Anna <34149510+shujhen@users.noreply.github.com>
2023-05-06 22:29:11 +0800

    [ fix ] 使用 next/link 替換 <a>

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import Image from 'next/image';
+import Link from 'next/link';
 import { MenuOutlined, CloseOutlined } from '@ant-design/icons';
 
-const MenuLink = (props) => {
+const MenuLink = function (props) {
   return (
     <li className="w-full border-t hover:bg-[#0083FF] hover:text-white">
-      <a href={`${props.href}`} className="block py-3">{props.title}</a>
+      <Link href={`${props.href}`} className="block py-3">{props.title}</Link>
     </li>
   )
 }
@@ -16,7 +17,7 @@ export default function Header() {
   return (
     <nav className="w-full fixed left-0 top-0 font-bold tracking-wide bg-[#FAF7F5]">
       <div className="flex items-center justify-between mx-3 xl:mx-[166px]">
-        <a className="py-3 lg:py-5" href="#">
+        <Link className="py-3 lg:py-5" href="#">
           <Image
             src="/logo.svg"
             alt="Lunar Logo"
@@ -25,7 +26,7 @@ export default function Header() {
             height={36}
             priority
           />
-        </a>
+        </Link>
         <MenuOutlined className={`cursor-pointer py-6 px-6 lg:hidden ${isNavOpen ? "hidden" : ""}`} onClick={() => setIsNavOpen((prev) => !prev)} />
         <CloseOutlined className={`cursor-pointer py-6 px-6 lg:hidden ${isNavOpen ? "" : "hidden"}`} onClick={() => setIsNavOpen(false)} />
         <div className={`absolute top-[64px] right-0 bg-[#FAF7F5] text-center w-full lg:hidden ${isNavOpen ? "" : "hidden"}`}>
@@ -39,13 +40,13 @@ export default function Header() {
         </div>
         <div className="hidden lg:flex flex-auto justify-between">
           <ul className="flex text-base">
-            <li><a href="#" className="py-7 px-5">關於 Lunar</a></li>
-            <li><a href="#" className="py-7 px-5">功能介紹</a></li>
-            <li><a href="#" className="py-7 px-5">聯繫我們</a></li>
+            <li><Link href="#" className="py-7 px-5">關於 Lunar</Link></li>
+            <li><Link href="#" className="py-7 px-5">功能介紹</Link></li>
+            <li><Link href="#" className="py-7 px-5">聯繫我們</Link></li>
           </ul>
           <ul className="flex text-sm">
-            <li><a className="py-2 px-4 mr-4" href="#">登入</a></li>
-            <li><a className="text-white py-2 px-4 bg-[#0083FF] rounded-md" href="#">開始使用</a></li>
+            <li><Link className="py-2 px-4 mr-4" href="#">登入</Link></li>
+            <li><Link className="text-white py-2 px-4 bg-[#0083FF] rounded-md" href="#">開始使用</Link></li>
           </ul>
         </div>
       </div>
